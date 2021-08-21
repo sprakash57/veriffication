602578bc529b984d1d318e8aff27dcc917a81038
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-08-21 18:33:50 +0530

    refactor: Try integrating the API

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
+import { CheckItem, fetchChecks } from '../helpers/api';
 import './styles/screens/App.modules.css';
 
-function App() {
+const App = () => {
+  const [checks, setChecks] = useState<CheckItem[]>([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const checks = await fetchChecks();
+      checks.sort((prev, next) => prev.priority - next.priority);
+      setChecks(checks);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError("Hmm... Something is not right!! Try refreshing the page.");
+    }
+  }
+  // Get the checklist at initial mount.
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       Veriffication
+      {JSON.stringify(checks)}
     </div>
   );
 }
